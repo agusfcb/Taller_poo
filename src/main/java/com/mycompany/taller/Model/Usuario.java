04fbca407b391d01186533c9d8a283c077bbd1f9
Agus <agustinfbc@gmail.com>
2024-10-27 04:10:26 -0300

    "Documentacion general"

--- src/main/java/com/mycompany/taller/Model/Usuario.java
+++ src/main/java/com/mycompany/taller/Model/Usuario.java
@@ -2,16 +2,13 @@ package com.mycompany.taller.Model;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.*;
 import javax.persistence.Basic;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
 /**
